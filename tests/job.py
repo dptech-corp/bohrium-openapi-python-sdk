49f3edb2d04260b8bca3a79d1e2f3d177d3b56f8
mahao <mahao@dp.tech>
2024-05-14 13:14:41 +0800

    update timeout

--- tests/job.py
+++ tests/job.py
@@ -1,10 +1,13 @@
 from bohrium import Bohrium
 
+import httpx
+
 if __name__ == "__main__":
 
     bohrium = Bohrium(
         access_key="access_key",
-        project_id="project_id"
+        project_id="project_id",
+        timeout=httpx.Timeout(40.0),
     )
 
     # job = bohrium.job.submit("project_id", "name")
